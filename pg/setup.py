1f889262b189bccbedeef537a191cb9ef41f32f3
Chris Austin <christopher.austin@gfk.com>
2022-06-06 14:30:22 +0100

    Trying setup locally

---
+++ pg/setup.py
@@ -0,0 +1,8 @@
+from distutils.ccompiler import new_compiler
+from distutils import sysconfig
+
+c = new_compiler()
+c.add_include_dir( sysconfig.get_python_inc() )
+c.add_include_dir("/usr/include/postgresql")
+objects = c.compile(["pg.c"], extra_postargs=["-fPIC"])
+c.link_shared_lib(objects, "pg", output_dir=".", libraries=["pq"])
