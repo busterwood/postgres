0c98b18534026be68733a9c91fd1debdaa1b5b17
Chris Austin <christopher.austin@gfk.com>
2022-06-06 11:27:22 +0100

    simplify Test

--- test.py
+++ test.py
@@ -1,60 +1,46 @@
-from __future__ import annotations # allow __enter__ to return Test
-from enum import Enum
+from __future__ import annotations  # allow __enter__ to return Test
 from types import TracebackType
 
 
 class FailNow(Exception):
     pass
 
+
 class SkipNow(Exception):
     pass
 
+
 class Test:
-    class State(Enum):
-        FAILED = 1,
-        SKIPPED = 2
-
     name: str
-    __state: State|None
-    __messages: list[str]
+    failed = False
+    skipped = False
+    __messages: list[str] = []
 
-    def __init__(self, test_name:str):
+    def __init__(self, test_name: str):
         self.name = test_name
-        self.__messages = []
-        self.__state = None
 
-    def log(self, message:str) -> None:
+    def log(self, message: str) -> None:
         """records information that is shown only if the test fails"""
         self.__messages.append(message)
 
-    def fail(self, message:str|None = None) -> None:
+    def fail(self, message: str | None = None) -> None:
         """marks the function as having failed but continues execution"""
         if message is not None:
             self.log(message)
-        self.__state = Test.State.FAILED
+        self.failed = True
 
-    def fail_now(self, message:str|None = None) -> None:
+    def fail_now(self, message: str | None = None) -> None:
         """marks the function as having failed and stops the test executing"""
         if message is not None:
             self.fail(message)
         raise FailNow()
 
-    def skip_now(self, message:str|None = None) -> None:
+    def skip_now(self, message: str | None = None) -> None:
         """skip_now() marks the test as having been skipped and stops its execution"""
         if message is not None:
             self.log(message)
-        self.__state = Test.State.SKIPPED
+        self.skipped = True
         raise SkipNow()
-
-    @property
-    def failed(self) -> bool:
-        """reports whether the test was failed"""
-        return self.__state == Test.State.FAILED
-
-    @property 
-    def skipped(self) -> bool:
-        """reports whether the test was skipped"""
-        return self.__state == Test.State.SKIPPED
 
     def report(self):
         if self.skipped:
@@ -79,17 +65,16 @@ class Test:
             self.fail()
 
         self.report()
-        return True # swallow exception
+        return True  # swallow exception
+
 
 with Test("can do stuff") as t:
     t.log("going ok")
-    # t.fail("bad, but continue")
+    t.fail("bad, but continue")
     t.log("maybe?")
-
-
 with Test("can do stuff") as t:
     pass
 with Test("can do stuff") as t:
     pass
 
-
+print()
